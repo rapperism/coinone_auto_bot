639d89bf705e02a6d91a2aac91c155ba92543a5d
smlee <rapperism@hotmail.com>
2026-04-18 07:58:36 +0900

    run.sh 보강

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -e
 source venv/Scripts/activate
+python -c "import requests" 2>/dev/null || pip install -r requirements.txt
 export PYTHONIOENCODING=utf-8
 export PYTHONUTF8=1
 python -X utf8 bot.py
